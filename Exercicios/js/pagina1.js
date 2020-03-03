04fbd8dfa1b9a4283402b7ded16a834ee4000acb
ERICK PORTUGAL FARIAS <erick.pfarias1@senacsp.edu.br>
2020-03-03 16:54:10 -0300

    site

---
+++ Exercicios/js/pagina1.js
@@ -0,0 +1,11 @@
+function retorno(){
+    var dom = prompt("Manifeste sua satisfação com a fantasia do presidente vampiro:");
+    alert(dom)
+}
+function troca(){
+    var nome = prompt("Digite seu nome aqui");
+    botao.textContent = ("Olá, " + nome + " como vai?")
+}
+function novo(){
+    var criar = textarea
+}
